ad0db38a2ad070644378a28fe5cac6b8a410d05f
andy <synckey@gmail.com>
2017-08-22 22:36:47 +0800

    add readme

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-sudo pip install beautifulsoup4
-sudo pip install markdown
-sudo pip install typogrify
-sudo pip install ghp-import
+sudo pip install beautifulsoup4 markdown typogrify ghp-import
 sudo pip install six
 
 git remote set-url --add origin https://git.coding.net/synckey/synckey.git
