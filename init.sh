304cdbb3cc79cd7cde3858acd95556a4224fd554
andy <synckey@gmail.com>
2017-09-20 17:59:49 +0800

    add the marathon post

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo pip install beautifulsoup4 markdown typogrify ghp-import
-sudo pip install six
+sudo pip install beautifulsoup4 markdown typogrify ghp-import six
 
 git remote set-url --add origin https://git.coding.net/synckey/synckey.git
