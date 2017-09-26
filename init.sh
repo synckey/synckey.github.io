4dd8a041e9e9d9fdc388d1a749957b16d612d697
andy <synckey@gmail.com>
2017-09-26 22:08:28 +0800

    fix the init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-sudo pip install beautifulsoup4 markdown typogrify ghp-import six
+sudo pip install beautifulsoup4 markdown typogrify ghp-import six pelican
 git remote set-url --add origin https://git.coding.net/synckey/synckey.git
+
