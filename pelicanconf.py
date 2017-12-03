6082e077080a9df5b9289184d7b4bdf45c0ca966
andy <synckey@gmail.com>
2017-12-03 19:47:28 +0800

    add useful set phrases

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,7 +44,6 @@ DEFAULT_PAGINATION = 30
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 DISQUS_SITENAME = u"synckey"
-# DUOSHUO_SITENAME = u"synckey"
 THEME = "pelican-bootstrap3"
 HIDE_SIDEBAR = True
 # BOOTSTRAP_FLUID =  True
@@ -57,7 +56,6 @@ EXTRA_PATH_METADATA = {
     "static/images/favicon.ico": {"path": "favicon.ico"},
     "static/CNAME": {"path": "CNAME"},
     "static/robots.txt": {"path": "robots.txt"}
-    #   "static/baidu_verify_eO5agZyv5R.html": {"path": "baidu_verify_eO5agZyv5R.html"},
 }
 
 SITEMAP = {
