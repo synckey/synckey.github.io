f2a6d95980580fd474147ef8901988d63a111d91
andy <synckey@gmail.com>
2018-02-06 18:24:18 +0800

    add ias 异步加载分页

--- publishconf.py
+++ publishconf.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
 sys.path.append(os.curdir)
-from pelicanconf import *
 
 TIMEZONE = 'Asia/Shanghai'
 SITEURL = 'http://synckey.name'
@@ -24,8 +23,9 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # DISQUS_SITENAME = "synckey"
 # GOOGLE_ANALYTICS = ""
-#DUOSHUO_SITENAME = u"synckey"
+# DUOSHUO_SITENAME = u"synckey"
 GOOGLE_ANALYTICS = "UA-55913313-1"
+GOOGLE_GMT = 'GTM-K3XTKKX'
 BAIDU_ANALYTICS = "5c7645d659d92aabca4e974b4aa61755"
 
 SHOW_DRAFTS = False
