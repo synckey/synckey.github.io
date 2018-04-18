13ff64f057e38a821b81151e34f93c46816ba09f
Jian Wang <andywan@amazon.com>
2018-04-17 20:53:10 -0700

    fix url bug

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo pip install beautifulsoup4 markdown typogrify ghp-import six pelican
+sudo pip3 install beautifulsoup4 markdown typogrify ghp-import six pelican
 git remote set-url --add origin https://git.coding.net/synckey/synckey.git
 
