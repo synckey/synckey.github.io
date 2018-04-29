0da621cf2193c21410e0812fee43f7fa305abd4b
jian <synckey@gmail.com>
2018-04-29 11:16:19 -0700

    add pelican conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,7 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = u'en'
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
